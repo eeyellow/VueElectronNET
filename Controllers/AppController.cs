f65cb05cf7f287fb9f7f61461f626e5678589064
黃敏睿 <rayhuang@linkchain.tw>
2024-06-21 13:52:32 +0800

    feat: 支援單頁式應用 Vite

---
+++ Controllers/AppController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ElectronApp.Controllers
+{
+    /// <summary>
+    /// 單頁式應用控制器
+    /// </summary>
+    public class AppController : BaseController
+    {
+        private readonly ILogger<AppController> _logger;
+        private readonly IWebHostEnvironment _env;
+
+        /// <summary>
+        /// 建構式
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="configuration"></param>
+        /// <param name="env"></param>
+        public AppController(ILogger<AppController> logger,
+                             IConfiguration configuration,
+                             IWebHostEnvironment env) 
+            : base(configuration)
+        {
+            _logger = logger;
+            _env = env;
+        }
+
+        /// <summary>
+        /// Index
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            // 讀取 VueApp/dist/index.html 的內容到stringbuilder
+            var indexHtml = System.IO.File.ReadAllText(
+                Path.Combine(_env.WebRootPath, "VueApp/index.html")
+            );
+
+            return Content(indexHtml, "text/html");
+        }
+    }
+}
---
+++ VueApp/vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? 'VueApp' : '',
+  plugins: [vue()],
+  build: {
+    outDir: '../wwwroot/VueApp',
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.').at(1);
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+          }
+          return `assets/${extType}/[name][extname]`;
+        },
+        chunkFileNames: 'assets/js/[name].js',
+        entryFileNames: 'assets/js/[name].js',
+      },
+    },
+  },
+})
